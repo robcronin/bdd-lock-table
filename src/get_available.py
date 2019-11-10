e4e42500bb6cca82e7a6694d2633453d8ce6aaf3
robcronin <robc@theodo.co.uk>
2019-11-10 23:14:24 +0000

    Query available stacks

---
+++ src/get_available.py
@@ -0,0 +1,23 @@
+import boto3
+import json
+import os
+from src.response import make_response
+from boto3.dynamodb.conditions import Key
+
+
+
+def query(event, context):
+    event_body = json.loads(event["body"])
+
+    repo_name = event_body["repoName"]
+
+    bdd_lock_table = os.environ["BDD_LOCK_TABLE_NAME"]
+    dynamodb = boto3.resource("dynamodb")
+    table = dynamodb.Table(bdd_lock_table)
+
+    results = table.query(
+        IndexName="availableByRepoIndex",
+        KeyConditionExpression=Key("repoName").eq(repo_name),
+    )
+
+    return make_response({"info": "Successfully queried", "results": results["Items"]})
