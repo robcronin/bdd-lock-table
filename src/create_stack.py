4e39f78f269b2a1f75425d9a2beccdf58ed87e0d
robcronin <robc@theodo.co.uk>
2019-11-10 22:36:56 +0000

    Add create stack endpoint

--- src/create_stack.py
+++ src/create_stack.py
@@ -1,9 +1,31 @@
+import boto3
 import json
+import os
 from src.response import make_response
 
 
 def create(event, context):
-    body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
+    event_body = json.loads(event["body"])
+    new_stack = {
+        "stackName": {"S": event_body["stackName"]},
+        "repoName": {"S": event_body["repoName"]},
     }
-    return make_response(body)
+    if(event_body["isAvailable"]):
+        new_stack["isAvailable"] = {"S": "x"}
+
+
+
+    client = boto3.client('dynamodb')
+    bdd_lock_table = os.environ["BDD_LOCK_TABLE_NAME"]
+    try:
+        client.put_item(
+            TableName=bdd_lock_table,
+            Item=new_stack,
+            ConditionExpression="attribute_not_exists(stackName)",
+        )
+    except client.exceptions.ConditionalCheckFailedException:
+        return make_response({"error": "Stack Name already exists"}, 400)
+    except Exception:
+        return make_response({"error": "Unknown Error"}, 400)
+
+    return make_response({"info": "Successfully created stack entry"})
