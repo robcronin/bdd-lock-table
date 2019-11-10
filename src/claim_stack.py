91e7c3647d223183884a3752898f75f0807dfadb
robcronin <robc@theodo.co.uk>
2019-11-10 23:53:03 +0000

    Add a release stack endpoint

--- src/claim_stack.py
+++ src/claim_stack.py
@@ -4,32 +4,46 @@ import os
 from src.response import make_response
 
 
+def transact_write(stack_name, update_type):
+
+    client = boto3.client("dynamodb")
+    bdd_lock_table = os.environ["BDD_LOCK_TABLE_NAME"]
+
+    update_body = {
+        "TableName": bdd_lock_table,
+        "Key": {"stackName": {"S": stack_name}},
+        "ExpressionAttributeValues": {":x": {"S": "x"},},
+    }
+
+    if update_type == "claim":
+        update_body["ConditionExpression"] = "isAvailable = :x"
+        update_body["UpdateExpression"] = "remove isAvailable"
+    elif update_type == "release":
+        update_body["ConditionExpression"] = "attribute_not_exists(isAvailable)"
+        update_body["UpdateExpression"] = "set isAvailable = :x"
+
+    try:
+        response = client.transact_write_items(TransactItems=[{"Update": update_body}])
+    except client.exceptions.TransactionCanceledException:
+        return make_response({"error": "Transaction Cancelled"}, 400)
+
+    return make_response(
+        {"info": f"Successfully {update_type}ed stack", "response": response}
+    )
+
+
 def claim(event, context):
     event_body = json.loads(event["body"])
 
     stack_name = event_body["stackName"]
 
-    client = boto3.client("dynamodb")
-    bdd_lock_table = os.environ["BDD_LOCK_TABLE_NAME"]
+    return transact_write(stack_name, "claim")
 
-    try:
-        response = client.transact_write_items(
-            TransactItems=[
-                {
-                    "Update": {
-                        "TableName": bdd_lock_table,
-                        "Key": {"stackName": {"S": stack_name}},
-                        "ConditionExpression": "isAvailable = :x",
-                        "UpdateExpression": "remove isAvailable",
-                        "ExpressionAttributeValues": {":x": {"S": "x"},},
-                    }
-                }
-            ]
-        )
-    except client.exceptions.TransactionCanceledException:
-        return make_response({"error": "Transaction Cancelled"}, 400)
 
-    return make_response(
-        {"info": "Successfully claimed stack", "response": response}
-    )
+def release(event, context):
+    event_body = json.loads(event["body"])
+
+    stack_name = event_body["stackName"]
+
+    return transact_write(stack_name, "release")
 
